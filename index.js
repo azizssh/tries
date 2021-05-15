616ffd8b3a30d64f12349b0786bb5524e8dbdf47
Aziz Abdullaev <aliqsson@gmail.com>
2021-05-15 17:31:24 -0500

    trie add

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const trie = Object.create(null);
 words.forEach((word) => {
   //traverse the word
   let curNode = trie;
-  for (let c of word) {
+  for (let i = 0; i < word.length; i++) {
+    const c = word[i];
     let letterVal = curNode[c];
     if (!letterVal) {
-      curNode[c] = { next: null };
-      curNode = curNode[c].next;
+      curNode[c] = {};
+    }
+    curNode = curNode[c];
+
+    if (i === word.length - 1) {
+      curNode.next = null;
     }
   }
 });
 
-console.log(trie);
+console.log(JSON.stringify(trie, null, 3));
